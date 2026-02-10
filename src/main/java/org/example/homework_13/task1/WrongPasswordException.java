b94e05250861d87dca6297b307fd37785289d9eb
Vitalikxc <vitalikzc@gmail.com>
2026-02-10 15:59:48 +0300

    Homework13

---
+++ src/main/java/org/example/homework_13/task1/WrongPasswordException.java
@@ -0,0 +1,10 @@
+package org.example.homework_13.task1;
+
+public class WrongPasswordException extends Exception {
+    public WrongPasswordException() {
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
