a59562ffd52102638e93050acf3a301898604172
Vitalikxc <vitalikzc@gmail.com>
2026-02-27 19:10:40 +0300

    Homework18

---
+++ src/main/java/org/example/homework_18/Main.java
@@ -0,0 +1,9 @@
+package org.example.homework_18;
+
+public class Main {
+    public static void main(String[] args) {
+        LearnStream learnStream = new LearnStream();
+        learnStream.task1();
+        learnStream.task2();
+    }
+}
